77375a5738d43c8394882bb42aeb3e07b22a3e52
d4ng3r228 <dangersof228@gmail.com>
2022-12-10 15:21:09 +0200

    DB created

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,35 @@ public class App
         car.setModel("A8L");
         car.setYear(2016);
         em.persist(car);
+
+
+        car.setBrand("Mercedes");
+        car.setModel("W 221");
+        car.setYear(2011);
+        em.persist(car);
+
+
+        car.setBrand("Lexus");
+        car.setModel("IS250");
+        car.setYear(2016);
+        em.persist(car);
+
+
+        car.setBrand("Toyota");
+        car.setModel("Land Cruiser");
+        car.setYear(2009);
+        em.persist(car);
+
+        car.setBrand("BMW");
+        car.setModel("M7");
+        car.setYear(2021);
+        em.persist(car);
+
+
+
+
         em.getTransaction().commit();
+
 
         em.close();
         emf.close();
