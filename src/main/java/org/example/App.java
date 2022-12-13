19ffc7dba64442dc4ab14285e5158007e80a7204
d4ng3r228 <dangersof228@gmail.com>
2022-12-13 16:27:56 +0200

    Some Shit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.mysql.cj.Session;
+import org.hibernate.Hibernate;
+import org.hibernate.jpa.HibernateQuery;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -10,20 +14,31 @@ public class App
 {
     public static void main( String[] args )
     {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("Hibernate_Try");
+
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
+
 
         Car car = new Car();
         car.setBrand("Audi");
         car.setModel("A8L");
         car.setYear(2016);
-        em.persist(car);
+        em.merge(car);
+
+
+        car.setBrand("Mercedes BENZ");
+        car.setModel("W 221");
+        car.setYear(2011);
+        em.merge(car);
         em.getTransaction().commit();
 
+        Query query = em.createQuery("select brand, model from Car where year > 2011");
+        List<Car> list = query.getResultList();
+        System.out.println(list);
         em.close();
         emf.close();
-        Query query = em.createQuery("select brand, model from Car where year > 2011");
-        List list = query.getResultList();
+
     }
 }
